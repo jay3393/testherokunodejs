a0961cd19f351d44db44afa614280cd6437eb929
Jason Liu <jhliu4@buffalo.edu>
2022-07-20 13:51:03 -0400

    h

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = process.env.BASE_URI;
+export const host = "";
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const setAvatarRoute = `${host}/api/auth/setAvatar`;
